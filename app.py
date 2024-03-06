70afb5b0bb86665fce897ee6243b24d8743ac3ce
Dhilaharis <126457555+Dhilaharis@users.noreply.github.com>
2024-03-06 12:33:52 +0000

    ab

--- app.py
+++ app.py
@@ -6,6 +6,62 @@ app = Flask(__name__)
 def home():
   return render_template('index.html')
 
+@app.route('/abicmtem')
+def abicmtem():
+  return render_template('abicmtem.html')
+
+@app.route('/ababtech')
+def ababtech():
+  return render_template('ababtech.html')
+
+@app.route('/abstella')
+def abstella():
+  return render_template('abstella.html')
+
+
+@app.route('/callforpaper')
+def callforpaper():
+  return render_template('callforpaper.html')
+
+@app.route('/scientific')
+def scientific():
+  return render_template('scientific.html')
+
+@app.route('/wca')
+def wca():
+  return render_template('wca.html')
+
+@app.route('/nta')
+def nta():
+  return render_template('nta.html')
+
+
+@app.route('/papersub')
+def papersub():
+  return render_template('papersub.html')
+
+
+@app.route('/presentationguide')
+def presentationguide():
+  return render_template('presentationguide.html')
+
+@app.route('/registration')
+def registration():
+  return render_template('registration.html')
+
+@app.route('/journals')
+def journals():
+  return render_template('journals.html')
+
+
+@app.route('/awards')
+def awards():
+  return render_template('awards.html')
+
+
+
+
+
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0',debug=False)
+  app.run(debug=True)
